daba07da0b5220c6e89b76df59227a8ae99bb9d8
AlexanderSergeevich <mrsashamartini@gmail.com>
2016-11-28 21:48:17 +0300

    create shopping cart without users

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :installs
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
